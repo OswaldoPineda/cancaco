ffa4f3944a0ba9286ba86dbccae9ac88b663876e
OswaldoPineda <carlos-1502010@hotmail.com>
2019-05-27 22:11:02 -0500

    Create the neighborhood field in the address table
    This adds the field of the neighborhood to the address table because
    it is a necessary field to know more about the location of the company.
    Since this field was not contemplated when the initial model was created.
    
    Co-authored-by: JPablo90 <jp.garciamurillo@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_221123) do
+ActiveRecord::Schema.define(version: 2019_05_28_030923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_221123) do
     t.bigint "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "neighborhood"
     t.index ["company_id"], name: "index_addresses_on_company_id"
   end
 
