2d2cfe09a7f5d09268f427c98482b2a42cfca408
OswaldoPineda <carlos-1502010@hotmail.com>
2019-05-29 11:18:30 -0500

    Add API to get data with zip code
    This adds the API of "codigos postales" to get automatically the
    city, state and neighborhoods belonging to the zip code.
    
    Co-authored-by: JPablo90 <jp.garciamurillo@gmail.com>

---
+++ app/assets/javascripts/pages/sign-up.js
@@ -0,0 +1,16 @@
+window.Canaco = window.Canaco || {};
+
+window.Canaco.Signup = {
+  init: function() {
+    $("#zip_code").blur(function(e) {
+      $.ajax({
+          url: 'https://api-codigos-postales.herokuapp.com/v2/codigo_postal/'+$("#zip_code").val(), 
+          type: 'GET',
+          success: function(response){
+            $("#city").val(response.municipio);
+            $("#state").val(response.estado);
+          }
+      });
+    });
+  }
+}
