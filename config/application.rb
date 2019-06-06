e52f568fe70e83e1d57b7f60a8b410ab3e577b41
OswaldoPineda <carlos-1502010@hotmail.com>
2019-06-06 13:29:48 -0500

    Configure the I18n gem to Spanish
    This configures the I18n gem to allow us to traslate
    the information and error messages from English
    to Spanish. Since the application will be used by
    Spanish speakers.
    
    Co-authored-by: JPablo90 <jp.garciamurillo@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module CanacoB2b
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.i18n.available_locales = [:es]
+    config.i18n.default_locale = :es
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
