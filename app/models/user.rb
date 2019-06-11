5e09d02b64e54cc20a4ba96e78fa231b9a73bd8d
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-06-11 17:50:39 -0500

    Lint models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :company
+  has_one :company, dependent: :destroy
   accepts_nested_attributes_for :company
-  validates :first_name, :last_name, length: { minimum: 2 } 
-  validates :first_name, :last_name, length: { maximum: 50 } 
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :first_name, :last_name, length: { maximum: 50 }
   validates :first_name, :last_name, :email, :password, presence: true
-  validates_format_of :email, with: Devise::email_regexp
+  validates :email, format: { with: Devise.email_regexp }
 end
