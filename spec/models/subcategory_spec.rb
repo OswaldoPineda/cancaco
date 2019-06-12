5000fa327b2ff00b9981da3ad5a7d39ef45317bd
LandoSot <danugfe@gmail.com>
2019-06-12 14:18:58 -0500

    Create models of categories and subcategories
    This create models to the user can search for
    categories any products, and adds the tests.
    
    Co-authored-by: Alan Anguiano <22619011+AlanAnguiano@users.noreplay.github.com>

---
+++ spec/models/subcategory_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Subcategory, type: :model do
+  describe 'Validate presences of fields' do
+    it { should validate_presence_of :title }
+  end
+  
+  describe 'Validate association' do
+    it { should belong_to(:category) }
+  end
+end
