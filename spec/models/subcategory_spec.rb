4e69cb68a4eac8870c3bbb667c1472076a3d5935
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-06-18 10:32:10 -0500

    Lint files

--- spec/models/subcategory_spec.rb
+++ spec/models/subcategory_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Subcategory, type: :model do
   describe 'Validate presences of fields' do
     it { should validate_presence_of :title }
   end
-  
+
   describe 'Validate association' do
     it { should belong_to(:category) }
   end
