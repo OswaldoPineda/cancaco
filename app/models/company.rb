589944b06cb82671483e32052489d3af65b25872
OswaldoPineda <carlos-1502010@hotmail.com>
2019-06-20 12:21:38 -0500

    Create migration to add a category in the company
    We create the association of the Company with Category to tag a
    company with a specific category, in this way, we can filter
    better to whom to show them the purchase requests.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :user
   has_one :address, dependent: :destroy
+  belongs_to :category, dependent: :destroy
   accepts_nested_attributes_for :address
   validates :affiliation, :name, presence: true
   validates :name, length: { minimum: 2, maximum: 100 }
---
+++ db/migrate/20190618234729_add_category_id_to_companies.rb
@@ -0,0 +1,5 @@
+class AddCategoryIdtoCompanies < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :companies, :category, index: true
+  end
+end
