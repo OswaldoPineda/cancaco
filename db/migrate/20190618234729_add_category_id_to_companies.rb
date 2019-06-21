53da7c44b5a1d78dc6cf467cf8352c82d64b0a67
OswaldoPineda <carlos-1502010@hotmail.com>
2019-06-21 11:43:21 -0500

    Fix add_category_id_to_companies migration name

--- db/migrate/20190618234729_add_category_id_to_companies.rb
+++ db/migrate/20190618234729_add_category_id_to_companies.rb
@@ -1,4 +1,4 @@
-class AddCategoryIdtoCompanies < ActiveRecord::Migration[5.2]
+class AddCategoryIdToCompanies < ActiveRecord::Migration[5.2]
   def change
     add_reference :companies, :category, index: true
   end
