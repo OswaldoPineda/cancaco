0358cbaa1c12f774f5926c46d24ca5c70b635624
OswaldoPineda <carlos-1502010@hotmail.com>
2019-06-21 17:31:32 -0500

    Fix signup tests
    This changes the form to receive the params in the "fill company"
    and "fill address" methods since the fills don't have values
    that receive in the params.
    in addition, the address word is corrected where they were wrong.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.feature 'Sign Up', type: :feature do
   given(:user) { FactoryBot.create(:user) }
   given(:company_id) { 'user[company_attributes]' }
-  given(:addres_id) { 'user[company_attributes][address_attributes]' }
+  given(:address_id) { 'user[company_attributes][address_attributes]' }
   background do
     visit root_path
     visit new_user_registration_path
@@ -16,17 +16,17 @@ RSpec.feature 'Sign Up', type: :feature do
     fill_in 'user[email]', with: 'new@test.com'
     fill_in 'user[password]', with: '123456'
     fill_in 'user[password_confirmation]', with: '123456'
-    fill_company_fields([
-      affiliation: '123456789',
+    fill_company_fields(
+      affiliation: '1234567890',
       name: 'Magmalabs'
-    ])
-    fill_address_fields([
+    )
+    fill_address_fields(
       zip_code: '29000',
       city: 'Colima',
       state: 'Colima',
       neighborhood: 'Centro',
       street: 'Aldama'
-    ])
+    )
     click_on 'Crear tu cuenta'
     expect(current_path).not_to eq(new_user_registration_path)
   end
@@ -36,17 +36,17 @@ RSpec.feature 'Sign Up', type: :feature do
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: '123456'
     fill_in 'user[password_confirmation]', with: '123456'
-    fill_company_fields([
-      affiliation: '123456789',
+    fill_company_fields(
+      affiliation: '1234567890',
       name: 'Magmalabs'
-    ])
-    fill_address_fields([
+    )
+    fill_address_fields(
       zip_code: '29000',
       city: 'Colima',
       state: 'Colima',
       neighborhood: 'Centro',
       street: 'Aldama'
-    ])
+    )
     expect(current_path).to eq(new_user_registration_path)
   end
 
@@ -55,17 +55,17 @@ RSpec.feature 'Sign Up', type: :feature do
     fill_in 'user[email]', with: 'new@test.com'
     fill_in 'user[password]', with: '1234'
     fill_in 'user[password_confirmation]', with: '1234'
-    fill_company_fields([
-      affiliation: '123456789',
+    fill_company_fields(
+      affiliation: '1234567890',
       name: 'Magmalabs'
-    ])
-    fill_address_fields([
+    )
+    fill_address_fields(
       zip_code: '29000',
       city: 'Colima',
       state: 'Colima',
       neighborhood: 'Centro',
       street: 'Aldama'
-    ])
+    )
     expect(current_path).to eq(new_user_registration_path)
   end
 
@@ -74,17 +74,17 @@ RSpec.feature 'Sign Up', type: :feature do
     fill_in 'user[email]', with: 'new@test.com'
     fill_in 'user[password]', with: '123456'
     fill_in 'user[password_confirmation]', with: '1234567'
-    fill_company_fields([
-      affiliation: '123456789',
+    fill_company_fields(
+      affiliation: '1234567890',
       name: 'Magmalabs'
-    ])
-    fill_address_fields([
+    )
+    fill_address_fields(
       zip_code: '29000',
       city: 'Colima',
       state: 'Colima',
       neighborhood: 'Centro',
       street: 'Aldama'
-    ])
+    )
     expect(current_path).to eq(new_user_registration_path)
   end
 
@@ -96,16 +96,16 @@ RSpec.feature 'Sign Up', type: :feature do
     expect(current_path).to eq(new_user_registration_path)
   end
 
-  def fill_company_fields(*)
-    fill_in "#{company_id}[affiliation]", with: 'params[:affiliation]'
-    fill_in "#{company_id}[name]", with: 'params[:name]'
+  def fill_company_fields(*params)
+    fill_in "#{company_id}[affiliation]", with: params[0][:affiliation]
+    fill_in "#{company_id}[name]", with: params[0][:name]
   end
 
-  def fill_address_fields(*)
-    fill_in "#{addres_id}[zip_code]", with: 'params[:zip_code]'
-    fill_in "#{addres_id}[city]", with: 'params[:city]'
-    fill_in "#{addres_id}[state]", with: 'params[:state]'
+  def fill_address_fields(*params)
+    fill_in "#{address_id}[zip_code]", with: params[0][:zip_code]
+    fill_in "#{address_id}[city]", with: params[0][:city]
+    fill_in "#{address_id}[state]", with: params[0][:state]
     first('#neighborhood option', minimum: 1).select_option
-    fill_in "#{addres_id}[street]", with: 'params[:street]'
+    fill_in "#{address_id}[street]", with: params[0][:street]
   end
 end
