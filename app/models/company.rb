3f8d043baa38e13f95bdb01e1d4adda7f18d4bfd
OswaldoPineda <carlos-1502010@hotmail.com>
2019-06-28 10:22:43 -0500

    Change validations in user and company models
    This changes the validations in the user and company
    models so that the password and the affiliation are
    only required in the create method since in the update
    they are not necessary.

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,8 +3,7 @@ class Company < ApplicationRecord
   has_one :user, dependent: :destroy
   belongs_to :category, dependent: :destroy
   accepts_nested_attributes_for :address
-  validates :affiliation, :name, presence: true
-  validates :name, length: { minimum: 2, maximum: 100 }
-  validates :affiliation, length: { is: 10 }
+  validates :name, length: { minimum: 2, maximum: 100 }, presence: true
+  validates :affiliation, presence: true, length: { is: 10 }, on: :create
   has_many :notifications, foreign_key: :recipient_id, dependent: :delete_all
 end
