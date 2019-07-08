071ed3f3f4ef29b4c45ad883f425e31a82afdc66
Alan Anguiano <alanmexico2020@hotmail.com>
2019-07-08 11:03:34 -0500

    Rename model and test

--- spec/factories/petition.rb
+++ spec/factories/petition.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :request do
-    title { 'Request of wheels' }
+  factory :petition do
+    title { 'Petition of wheels' }
     description { 'required wheels of bus' }
     quantity { 5 }
     deadline { '2019-06-30' }
