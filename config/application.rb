85bd799c23caeaf9d02201eb354b6e1eb678a511
JPablo90 <jp.garciamurillo@gmail.com>
2019-07-10 12:59:58 -0500

    Configurate the middleware in rails
    Is necesary to modify the routes to avoid have
    Rails’s default error pages, since these are boring
    and bland.
    The hook of Rails is modified to creating a custom
    error pages (404, 422 and 500).
    Also, a new controller and its unit test are created.
    This controller is in charge to redirect to the new
    views and follow the configuration to custom the
    error pages.
    Finally the new error views are created and the default
    views are removed, the application view is modified to
    remove the header and navigation bar from error views.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CanacoB2b
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.exceptions_app = routes
 
     config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
