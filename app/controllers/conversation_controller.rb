8bdf4ef1c9b38bf17636f032e232768ca739c5cc
Alan Anguiano <alanmexico2020@hotmail.com>
2019-08-01 11:18:40 -0500

    Delete misnamed files
    Deletes misnamed files that do not follow ruby on rails
    conventions, also, removed a relation to subject that was
    deleted before.

--- app/controllers/conversation_controller.rb
+++
@@ -1,5 +0,0 @@
-class ConversationController < ApplicationController
-  before_action :authenticate_user!
-
-  def index; end
-end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,4 @@
 class Conversation < ApplicationRecord
   belongs_to :sender, class_name: :Company
   belongs_to :recipient, class_name: :Company
-  belongs_to :subject, polymorphic: true
 end
