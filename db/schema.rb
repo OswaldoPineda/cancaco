3c4c842762f27ca4c04e0e6c6b06ad0883e6e18b
OswaldoPineda <carlos-1502010@hotmail.com>
2019-08-20 16:35:13 -0500

    Add status to sales migration
    This adds a status to the sale table to can know if the sale is
    currently available.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_213725) do
+ActiveRecord::Schema.define(version: 2019_08_19_205104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,11 @@ ActiveRecord::Schema.define(version: 2019_08_03_213725) do
     t.bigint "subcategory_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "document_file_name"
+    t.string "document_content_type"
+    t.bigint "document_file_size"
+    t.datetime "document_updated_at"
+    t.boolean "active", default: true
     t.index ["category_id"], name: "index_sales_on_category_id"
     t.index ["company_id"], name: "index_sales_on_company_id"
     t.index ["subcategory_id"], name: "index_sales_on_subcategory_id"
@@ -140,7 +145,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_213725) do
     t.bigint "company_id"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
-    t.integer "avatar_file_size"
+    t.bigint "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
