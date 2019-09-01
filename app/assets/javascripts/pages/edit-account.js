ff35a85ed3ac95ef236936eb36a869c0e1c39cde
OswaldoPineda <carlos-1502010@hotmail.com>
2019-09-01 14:55:57 -0500

    Create image previewer
    This creates the image previewer to the edit account view to  can
    preview the avatar that the user upload in real-time.

--- app/assets/javascripts/pages/edit-account.js
+++ app/assets/javascripts/pages/edit-account.js
@@ -1,9 +1,25 @@
-$('[type=file]').on('change', function(){
-  var file = this.files[0].name;
-  var dflt = $(this).attr('placeholder');
-  if($(this).val()!==''){
-    $(this).next().text(file);
-  } else {
-    $(this).next().text(dflt);
+$(document).ready(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('#img-upload').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
   }
+
+  $('.img-input').change(function(){
+    readURL(this);
+  });
+
+  $('[type=file]').on('change', function(){
+    var file = this.files[0].name;
+    var dflt = $(this).attr('placeholder');
+    if($(this).val()!==''){
+      $(this).next().text(file);
+    } else {
+      $(this).next().text(dflt);
+    }
+  });
 });
