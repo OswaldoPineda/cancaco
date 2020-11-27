c50b56ca3ab47b0baaf8f8aa401f703700d9d28e
Alan Anguiano <alanmexico2020@hotmail.com>
2020-11-26 20:23:42 -0600

    test v343

--- app/assets/javascripts/pages/sign-up.js
+++ app/assets/javascripts/pages/sign-up.js
@@ -4,9 +4,6 @@ window.Canaco.Signup = {
   init: function() {
     var zipCode = $('#zip_code').val().trim();
     $('#zip_code').blur(fillData(zipCode));
-    if (zipCode.length > 0) {
-      fillData(zipCode);
-    }
   }
 }
 
@@ -14,27 +11,31 @@ var fillData = function(zipCode){
   console.log('before debugger')
   debugger
   console.log("zipcode: "+zipCode)
-  $.ajax({
-    url: 'https://api-sepomex.hckdrk.mx/query/info_cp/' + zipCode,
-    type: 'GET', 
-    cors: true ,
-    contentType:'application/json',
-    secure: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    success: function(response){
-      var response = body[0].response;
-      var municipio = response.municipio;
-      var estado = response.estado;
-      var city = document.getElementById('city');
-      var state = document.getElementById('state');
-      city.value = municipio;
-      state.value = estado;
-      body.forEach(function (value) {
-        var colonia = value.response.asentamiento;
-        $('#neighborhood').append('<option value="'+ colonia +'">' + colonia + '</option>');
-      });
-    }
-  });
+
+  if (zipCode) {
+    $.ajax({
+      url: 'https://api-sepomex.hckdrk.mx/query/info_cp/' + zipCode,
+      type: 'GET', 
+      cors: true ,
+      contentType:'application/json',
+      secure: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      success: function(response){
+        var response = body[0].response;
+        var municipio = response.municipio;
+        var estado = response.estado;
+        var city = document.getElementById('city');
+        var state = document.getElementById('state');
+        city.value = municipio;
+        state.value = estado;
+        body.forEach(function (value) {
+          var colonia = value.response.asentamiento;
+          $('#neighborhood').append('<option value="'+ colonia +'">' + colonia + '</option>');
+        });
+      }
+    });
+  }
 }
+
